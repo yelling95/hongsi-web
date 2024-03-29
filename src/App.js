9dbfc6838012e8d8789399be7605268fac154a2b
veronica <veronica.seo@companywe.co.kr>
2024-03-29 14:32:30 +0900

    소스 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {MainView} from './pages'
+import {HomeView} from './pages'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 export default function App(props) {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <MainView />,
+      element: <HomeView />,
     },
   ])
 
---
+++ src/pages/Home/HomeView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Home.scss'
+
+function HomeView(props) {
+  return <div className="home_container">ddd</div>
+}
+
+export default HomeView
