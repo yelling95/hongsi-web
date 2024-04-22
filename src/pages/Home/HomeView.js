bbb0fc9cd51ab31bd7411e02cf48d22445232149
veronica <veronica.seo@companywe.co.kr>
2024-04-22 17:35:45 +0900

    피드 메인 완료

--- src/pages/Home/HomeView.js
+++ src/pages/Home/HomeView.js
@@ -1,25 +1,76 @@
 import React, {useState} from 'react'
-import {HomeHeader, Sheet, Feed, Footer, SortHeader} from 'hongsi-ui'
+import Slider from 'react-slick'
+import {HomeHeader, Sheet, Footer, SortHeader, Feed, Dropdown} from 'hongsi-ui'
 import {Menus} from './const'
-import {sampleFeedList} from './structure'
+import {SliderOpt, SampleFeedList, SampleFeedFilterList, SampleFeedOrderList} from './structure'
 import {map} from 'lodash-es'
 
+import './Slick.scss'
 import './Home.scss'
 
 function HomeView(props) {
-  const [feedList, setFeedList] = useState(sampleFeedList)
+  const [feedList, setFeedList] = useState(SampleFeedList)
+  const [orderOptions, setOrderOptions] = useState(SampleFeedOrderList())
+  const [filterOptions, setFilterOptions] = useState(SampleFeedFilterList())
+  const [order, setOrder] = useState(orderOptions[0])
+  const [filter, setFilter] = useState(filterOptions[0])
+  const [isOpenOrder, setOpenOrder] = useState(false)
+  const [isOpenFilter, setOpenFilter] = useState(false)
 
   return (
     <div className="home_container">
       <HomeHeader />
       <Sheet message="텍스트는 <br/>최대 두 줄까지 적어주세요" />
-      <SortHeader />
+      <SortHeader
+        order={order?.label}
+        filter={filter?.label}
+        onClickOrder={() => setOpenOrder(true)}
+        onClickFilter={() => setOpenFilter(true)}
+      />
       <div className="scroll_wrap">
         {map(feedList, (feed) => (
-          <Feed imgList={feed.imgList} />
+          <Feed key={`feed-${feed.id}`}>
+            {feed.imageList && feed.imageList.length > 0 && (
+              <div className="slider_wrap" style={{height: 540}}>
+                <Slider key={`slider-${feed.id}`} {...SliderOpt}>
+                  {map(feed.imageList, (img, index) => (
+                    <div key={`slider-img-${index}`} className="img_wrap">
+                      <div style={{height: 540, backgroundImage: `url('${img.url}')`}}></div>
+                    </div>
+                  ))}
+                </Slider>
+              </div>
+            )}
+          </Feed>
         ))}
       </div>
       <Footer menuList={Menus} />
+      <Dropdown
+        id="order"
+        isShow={isOpenOrder}
+        isShowDimm={true}
+        selected={order}
+        close={() => setOpenOrder(false)}
+        options={map(orderOptions, (o) => ({
+          ...o,
+          click: () => {
+            alert(o.id)
+          },
+        }))}
+      />
+      <Dropdown
+        id="filter"
+        isShow={isOpenFilter}
+        isShowDimm={true}
+        selected={filter}
+        close={() => setOpenFilter(false)}
+        options={map(filterOptions, (o) => ({
+          ...o,
+          click: () => {
+            alert(o.id)
+          },
+        }))}
+      />
     </div>
   )
 }
--- src/pages/Home/structure.js
+++ src/pages/Home/structure.js
@@ -1,17 +1,75 @@
 import {range} from 'lodash-es'
 
-export const sampleFeedList = range(1, 11, 1).map((dummy) => {
+export const SliderOpt = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+}
+
+export const DropdownOpt = {
+  id: '',
+  label: '',
+  click: () => {},
+}
+
+export const SliderImage = {
+  url: '',
+  desc: '',
+}
+
+export const SampleFeedFilterList = () => {
+  return [
+    '전체',
+    '일상',
+    '취미생활',
+    '여행 · 맛집',
+    '건강 · 다이어트',
+    '운동 · 스포츠',
+    '재태크 · 부동산',
+    '문화 · 예술',
+    '연애 · 결혼',
+    '채용 · 커리어',
+    '봉사활동',
+    '같이해요',
+  ].map((keyword, index) => {
+    return {
+      id: index,
+      label: keyword,
+    }
+  })
+}
+
+export const SampleFeedOrderList = () => {
+  return ['최신순', '인기순', '추천순'].map((keyword, index) => {
+    return {
+      id: index,
+      label: keyword,
+    }
+  })
+}
+
+export const SampleFeedList = range(1, 11, 1).map((dummy) => {
   return {
+    id: dummy,
     profileUrl: '',
     username: '홍길동 ' + dummy,
     category: '',
     timing: '',
-    imgList: [],
     title: '제목 ' + dummy,
     subtitle: '소제목 ' + dummy,
     likeCnt: 10,
     chatCnt: 3,
     gather: false,
     declare: false,
+    imageList:
+      dummy % 2 === 0
+        ? range(1, 11, 1).map((n) => ({
+            url: 'https://cdn.eroun.net/news/photo/201904/5248_19828_3216.jpg',
+            desc: '사진 ' + n,
+          }))
+        : [],
   }
 })
